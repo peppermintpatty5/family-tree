f47f67d9cc582174fe04b59ae91ac528b2e5ed65
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2023-11-21 13:30:06 -0500

    Gendered relationship labels

--- family.py
+++ family.py
@@ -16,6 +16,10 @@ class Gender(Enum):
 
 @dataclass
 class Person:
+    """
+    Represents the basic genealogical information of a person.
+    """
+
     id: int
     first_name: str
     last_name: str
@@ -24,10 +28,52 @@ class Person:
     father: "Person | None" = None
 
 
+class BaseRelative(Enum):
+    """
+    A helper enumeration for constructing the 3x3 matrix of basic relatives.
+    """
+
+    SELF = ("self", None, None)
+    CHILD = ("child", "son", "daughter")
+    GRANDCHILD = ("grandchild", "grandson", "granddaughter")
+    PARENT = ("parent", "father", "mother")
+    SIBLING = ("sibling", "brother", "sister")
+    NIECE_OR_NEPHEW = (None, "nephew", "niece")
+    GRANDPARENT = ("grandparent", "grandfather", "grandmother")
+    AUNT_OR_UNCLE = (None, "uncle", "aunt")
+    COUSIN = ("cousin", None, None)
+
+    def get_label(self, gender: Gender | None) -> str:
+        """
+        Get the relative's corresponding gendered or non-gendered English label as a
+        lowercase string.
+        """
+        neutral, male, female = self.value
+        label = (
+            male or neutral
+            if gender is Gender.MALE
+            else female or neutral
+            if gender is Gender.FEMALE
+            else neutral or f"{female} or {male}"
+        )
+        assert label is not None
+        return label
+
+
 @dataclass
 class Relationship:
+    """
+    Represents a relationship between two family members.
+
+    A relationship is defined by the relative position of the closest common ancestor.
+    For example, you and your aunt/uncle are related by your grandparents. The path is
+    two generations up from yourself and then one generation down. "2 up, 1 down", or
+    `(2, 1)`, uniquely represents the aunt/uncle relationship.
+    """
+
     up: int
     down: int
+    gender: Gender | None
 
     @classmethod
     def of(cls, person1: Person, person2: Person) -> Self:
@@ -39,7 +85,7 @@ class Relationship:
             if queue1:
                 person, depth = queue1.popleft()
                 if person.id in visited:
-                    return cls(depth, visited[person.id])
+                    return cls(depth, visited[person.id], person2.gender)
 
                 visited[person.id] = depth
                 if person.mother is not None:
@@ -49,7 +95,7 @@ class Relationship:
             if queue2:
                 person, depth = queue2.popleft()
                 if person.id in visited:
-                    return cls(visited[person.id], depth)
+                    return cls(visited[person.id], depth, person2.gender)
 
                 visited[person.id] = depth
                 if person.mother is not None:
@@ -57,36 +103,60 @@ class Relationship:
                 if person.father is not None:
                     queue2.append((person.father, depth + 1))
 
-        return cls(-1, -1)
+        return cls(-1, -1, person2.gender)
 
-    def get_description(self) -> str:
-        match (self.up, self.down):
-            case (x, y) if x < 0 and y < 0:
-                return "unrelated"
-            case (0, 0):
-                return "self"
-            case (0, 1):
-                return "child"
-            case (0, 2):
-                return "grandchild"
-            case (0, 3):
-                return "great grandchild"
-            case (1, 0):
-                return "parent"
-            case (1, 1):
-                return "sibling"
-            case (1, 2):
-                return "niece/nephew"
-            case (2, 0):
-                return "grandparent"
-            case (2, 1):
-                return "aunt/uncle"
-            case (2, 2):
-                return "cousin"
-            case (3, 0):
-                return "great grandparent"
-            case _:
-                return ""
+    def get_label(self) -> str:
+        R = BaseRelative  # less typing
+        ordinals = [
+            "first",
+            "second",
+            "third",
+            "fourth",
+            "fifth",
+            "sixth",
+            "seventh",
+            "eighth",
+            "ninth",
+            "tenth",
+        ]
+        reps = ["once", "twice", "thrice"]
+        base_relatives_matrix = (
+            (R.SELF, R.CHILD, R.GRANDCHILD),
+            (R.PARENT, R.SIBLING, R.NIECE_OR_NEPHEW),
+            (R.GRANDPARENT, R.AUNT_OR_UNCLE, R.COUSIN),
+        )
+
+        match self.up, self.down:
+            case (up, down) if up < 0 or down < 0:
+                label = "unrelated"
+            case (up, down) if up < 3 and down < 3:
+                label = base_relatives_matrix[up][down].get_label(self.gender)
+            case (0, down):
+                label = "great " * (down - 2) + R.GRANDCHILD.get_label(self.gender)
+            case (1, down):
+                label = (
+                    "great " * (down - 3)
+                    + "grand "
+                    + R.NIECE_OR_NEPHEW.get_label(self.gender)
+                )
+            case (up, 0):
+                label = "great " * (up - 2) + R.GRANDPARENT.get_label(self.gender)
+            case (up, 1):
+                label = (
+                    "great " * max(1, up - 3)
+                    + "grand " * (up > 3)
+                    + R.AUNT_OR_UNCLE.get_label(self.gender)
+                )
+            case (up, down):
+                r = abs(up - down)
+                removal = reps[r - 1] if 0 <= r - 1 < len(reps) else f"{r} times"
+                label = (
+                    f"{ordinals[min(up, down) - 2]} "
+                    + R.COUSIN.get_label(self.gender)
+                    + (f" {removal} removed" if r != 0 else "")
+                )
+
+        return label
 
 
 @dataclass
--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python3
 
-from itertools import product
-import sys
+from family import Relationship
 
-from family import Family, Relationship
-
-family = Family.from_csv(sys.stdin)
-
-if family.is_valid():
-    table = [
-        (
-            f"{person1.first_name} {person1.last_name}",
-            f"{person2.first_name} {person2.last_name}",
-            Relationship.of(person1, person2).get_description(),
-        )
-        for person1, person2 in product(family.members.values(), repeat=2)
-    ]
-
-    for row in table:
-        print("\t".join(row))
+table = [
+    [Relationship(up, down, None).get_label() for up in range(6)] for down in range(6)
+]
+for row in table:
+    print("\t".join(row))
