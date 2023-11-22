29e3b94c24e62c953187e11a800848fc286d0960
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2023-11-21 20:36:04 -0500

    Better ordinals

--- family.py
+++ family.py
@@ -43,7 +43,7 @@ class BaseRelative(Enum):
     AUNT_OR_UNCLE = (None, "uncle", "aunt")
     COUSIN = ("cousin", None, None)
 
-    def get_label(self, gender: Gender | None) -> str:
+    def label(self, gender: Gender | None) -> str:
         """
         Get the relative's corresponding gendered or non-gendered English label as a
         lowercase string.
@@ -105,58 +105,53 @@ class Relationship:
 
         return cls(-1, -1, person2.gender)
 
-    def get_label(self) -> str:
+    def label(self) -> str:
         R = BaseRelative  # less typing
-        ordinals = [
-            "first",
-            "second",
-            "third",
-            "fourth",
-            "fifth",
-            "sixth",
-            "seventh",
-            "eighth",
-            "ninth",
-            "tenth",
-        ]
-        reps = ["once", "twice", "thrice"]
         base_relatives_matrix = (
             (R.SELF, R.CHILD, R.GRANDCHILD),
             (R.PARENT, R.SIBLING, R.NIECE_OR_NEPHEW),
             (R.GRANDPARENT, R.AUNT_OR_UNCLE, R.COUSIN),
         )
 
+        def great(n: int):
+            return (
+                ""
+                if n <= 0
+                else "great"
+                if n == 1
+                else "great great"
+                if n == 2
+                else f"{(_ordinal(n))} great"
+            )
+
+        def removed(n: int) -> str:
+            if n > 0:
+                times = ["once", "twice", "thrice"][n - 1] if n <= 3 else f"{n} times"
+                return f"{times} removed"
+            return ""
+
+        g = self.gender
         match self.up, self.down:
             case (up, down) if up < 0 or down < 0:
                 label = "unrelated"
             case (up, down) if up < 3 and down < 3:
-                label = base_relatives_matrix[up][down].get_label(self.gender)
+                label = base_relatives_matrix[up][down].label(g)
             case (0, down):
-                label = "great " * (down - 2) + R.GRANDCHILD.get_label(self.gender)
+                label = f"{great(down - 2)} {R.GRANDCHILD.label(g)}"
             case (1, down):
-                label = (
-                    "great " * (down - 3)
-                    + "grand "
-                    + R.NIECE_OR_NEPHEW.get_label(self.gender)
-                )
+                label = f"{great(down - 3)} grand {R.NIECE_OR_NEPHEW.label(g)}"
             case (up, 0):
-                label = "great " * (up - 2) + R.GRANDPARENT.get_label(self.gender)
+                label = f"{great(up - 2)} {R.GRANDPARENT.label(g)}"
             case (up, 1):
                 label = (
-                    "great " * max(1, up - 3)
-                    + "grand " * (up > 3)
-                    + R.AUNT_OR_UNCLE.get_label(self.gender)
+                    f"{great(max(1, up - 3))}{" grand" * (up > 3)} {R.AUNT_OR_UNCLE.label(g)}"
                 )
             case (up, down):
-                r = abs(up - down)
-                removal = reps[r - 1] if 0 <= r - 1 < len(reps) else f"{r} times"
                 label = (
-                    f"{ordinals[min(up, down) - 2]} "
-                    + R.COUSIN.get_label(self.gender)
-                    + (f" {removal} removed" if r != 0 else "")
+                    f"{_ordinal(min(up, down) - 1)} {R.COUSIN.label(g)} {removed(abs(up - down))}"
                 )
 
-        return label
+        return label.strip()
 
 
 @dataclass
@@ -201,3 +196,13 @@ class Family:
             for person in self.members.values()
             if person.mother is not None
         )
+
+
+def _ordinal(n: int) -> str:
+    """
+    Returns the corresponding ordinal for a given non-negative integer.
+    """
+    suffix = (
+        "th" if 11 <= n % 100 <= 13 else ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
+    )
+    return str(n) + suffix
--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 
 from family import Relationship
 
-table = [
-    [Relationship(up, down, None).get_label() for up in range(6)] for down in range(6)
-]
+table = [[Relationship(up, down, None).label() for up in range(6)] for down in range(6)]
 for row in table:
     print("\t".join(row))
