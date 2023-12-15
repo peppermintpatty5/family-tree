7a2654968ca5e4d6dd5fc7e00e1b981fc6dae39b
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2023-12-15 00:31:41 -0500

    Use dictionary instead of nodes for adjacencies

--- family.py
+++ family.py
@@ -1,8 +1,6 @@
 from collections import deque
-import csv
 from dataclasses import dataclass
 from enum import Enum
-from typing import Iterable, Self
 
 
 class Gender(Enum):
@@ -14,18 +12,18 @@ class Gender(Enum):
     FEMALE = 1
 
 
-@dataclass
+@dataclass(frozen=True)
 class Person:
     """
     Represents the basic genealogical information of a person.
     """
 
-    id: int
+    id: str
     first_name: str
     last_name: str
     gender: Gender
-    mother: "Person | None" = None
-    father: "Person | None" = None
+    mother_id: str | None = None
+    father_id: str | None = None
 
     def __repr__(self) -> str:
         return f"{self.first_name} {self.last_name} ({self.gender.name})"
@@ -80,74 +78,6 @@ class Relationship:
     down: int
     half: bool = False
 
-    @classmethod
-    def of(cls, person1: Person, person2: Person) -> Self:
-        """
-        Determine the relationship of two people by performing a breadth-first search of
-        both persons' ancestors.
-        """
-        if person1 is person2:
-            return cls(person1, person2, 0, 0)
-
-        queue1: deque[tuple[Person, int]] = deque([(person1, 0)])
-        queue2: deque[tuple[Person, int]] = deque([(person2, 0)])
-        visited: dict[int, int] = {}
-
-        while queue1 or queue2:
-            if queue1:
-                a, depth = queue1.popleft()
-
-                if a.mother is person2 or a.father is person2:
-                    return cls(person1, person2, depth + 1, 0)
-
-                mother_depth, father_depth = (
-                    visited.get(a.mother.id, -1) if a.mother is not None else -1,
-                    visited.get(a.father.id, -1) if a.father is not None else -1,
-                )
-                if mother_depth >= 0 or father_depth >= 0:
-                    return cls(
-                        person1,
-                        person2,
-                        depth + 1,
-                        max(mother_depth, father_depth),
-                        half=mother_depth < 0 or father_depth < 0,
-                    )
-
-                if a.mother is not None:
-                    queue1.append((a.mother, depth + 1))
-                    visited[a.mother.id] = depth + 1
-                if a.father is not None:
-                    queue1.append((a.father, depth + 1))
-                    visited[a.father.id] = depth + 1
-
-            if queue2:
-                a, depth = queue2.popleft()
-
-                if a.mother is person1 or a.father is person1:
-                    return cls(person1, person2, 0, depth + 1)
-
-                mother_depth, father_depth = (
-                    visited.get(a.mother.id, -1) if a.mother is not None else -1,
-                    visited.get(a.father.id, -1) if a.father is not None else -1,
-                )
-                if mother_depth >= 0 or father_depth >= 0:
-                    return cls(
-                        person1,
-                        person2,
-                        max(mother_depth, father_depth),
-                        depth + 1,
-                        half=mother_depth < 0 or father_depth < 0,
-                    )
-
-                if a.mother is not None:
-                    queue2.append((a.mother, depth + 1))
-                    visited[a.mother.id] = depth + 1
-                if a.father is not None:
-                    queue2.append((a.father, depth + 1))
-                    visited[a.father.id] = depth + 1
-
-        return cls(person1, person2, -1, -1)
-
     def label(self) -> str:
         R = BaseRelative  # less typing
         base_relatives_matrix = (
@@ -193,32 +123,21 @@ class Relationship:
         return ("half " if self.half else "") + label.strip()
 
 
-@dataclass
 class Family:
-    members: dict[int, Person]
+    def __init__(self, people: list[Person]) -> None:
+        self.members = {person.id: person for person in people}
 
-    @classmethod
-    def from_csv(cls, csv_file: Iterable[str]) -> Self:
-        graph = {
-            int(id, base=16): (
-                Person(int(id, base=16), first_name, last_name, Gender[gender]),
-                int(mother_id, base=16) if mother_id != "" else None,
-                int(father_id, base=16) if father_id != "" else None,
-            )
-            for (
-                id,
-                first_name,
-                last_name,
-                gender,
-                mother_id,
-                father_id,
-            ) in csv.reader(csv_file)
-        }
-        for person, mother_id, father_id in graph.values():
-            person.mother = graph[mother_id][0] if mother_id is not None else None
-            person.father = graph[father_id][0] if father_id is not None else None
+    def mother(self, person: Person) -> Person | None:
+        """
+        Returns the mother of the given person.
+        """
+        return self.members[person.mother_id] if person.mother_id is not None else None
 
-        return cls({person.id: person for person, _, _ in graph.values()})
+    def father(self, person: Person) -> Person | None:
+        """
+        Returns the father of the given person.
+        """
+        return self.members[person.father_id] if person.father_id is not None else None
 
     def is_valid(self) -> bool:
         """
@@ -227,14 +146,83 @@ class Family:
         - All mothers are female
         """
         return all(
-            person.father.gender is Gender.MALE
-            for person in self.members.values()
-            if person.father is not None
+            father.gender is Gender.MALE
+            for father in map(self.father, self.members.values())
+            if father is not None
         ) and all(
-            person.mother.gender is Gender.FEMALE
-            for person in self.members.values()
-            if person.mother is not None
+            mother.gender is Gender.FEMALE
+            for mother in map(self.mother, self.members.values())
+            if mother is not None
         )
+
+    def relationship(self, person1: Person, person2: Person) -> Relationship:
+        """
+        Determine the relationship of two people by performing a breadth-first search of
+        both persons' ancestors.
+        """
+        if person1 is person2:
+            return Relationship(person1, person2, 0, 0)
+
+        queue1: deque[tuple[Person, int]] = deque([(person1, 0)])
+        queue2: deque[tuple[Person, int]] = deque([(person2, 0)])
+        visited: dict[str, int] = {}
+
+        while queue1 or queue2:
+            if queue1:
+                a, depth = queue1.popleft()
+                mother_a, father_a = self.mother(a), self.father(a)
+
+                if mother_a is person2 or father_a is person2:
+                    return Relationship(person1, person2, depth + 1, 0)
+
+                mother_depth, father_depth = (
+                    visited.get(mother_a.id, -1) if mother_a is not None else -1,
+                    visited.get(father_a.id, -1) if father_a is not None else -1,
+                )
+                if mother_depth >= 0 or father_depth >= 0:
+                    return Relationship(
+                        person1,
+                        person2,
+                        depth + 1,
+                        max(mother_depth, father_depth),
+                        half=mother_depth < 0 or father_depth < 0,
+                    )
+
+                if mother_a is not None:
+                    queue1.append((mother_a, depth + 1))
+                    visited[mother_a.id] = depth + 1
+                if father_a is not None:
+                    queue1.append((father_a, depth + 1))
+                    visited[father_a.id] = depth + 1
+
+            if queue2:
+                a, depth = queue2.popleft()
+                mother_a, father_a = self.mother(a), self.father(a)
+
+                if mother_a is person1 or father_a is person1:
+                    return Relationship(person1, person2, 0, depth + 1)
+
+                mother_depth, father_depth = (
+                    visited.get(mother_a.id, -1) if mother_a is not None else -1,
+                    visited.get(father_a.id, -1) if father_a is not None else -1,
+                )
+                if mother_depth >= 0 or father_depth >= 0:
+                    return Relationship(
+                        person1,
+                        person2,
+                        max(mother_depth, father_depth),
+                        depth + 1,
+                        half=mother_depth < 0 or father_depth < 0,
+                    )
+
+                if mother_a is not None:
+                    queue2.append((mother_a, depth + 1))
+                    visited[mother_a.id] = depth + 1
+                if father_a is not None:
+                    queue2.append((father_a, depth + 1))
+                    visited[father_a.id] = depth + 1
+
+        return Relationship(person1, person2, -1, -1)
 
 
 def _ordinal(n: int) -> str:
--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python3
 
+from collections.abc import Iterable
+import csv
 import sys
-from family import Family, Relationship
 
-family = Family.from_csv(sys.stdin)
+from family import Family, Gender, Person
+
+
+def from_csv(csv_file: Iterable[str]) -> list[Person]:
+    """
+    Read a list of people from a CSV file.
+    """
+    return [
+        Person(
+            id,
+            first_name,
+            last_name,
+            Gender[gender],
+            mother_id if mother_id != "" else None,
+            father_id if father_id != "" else None,
+        )
+        for (id, first_name, last_name, gender, mother_id, father_id) in csv.reader(
+            csv_file
+        )
+    ]
+
+
+people = from_csv(sys.stdin)
+family = Family(people)
 relationships = (
-    Relationship.of(person1, person2)
+    family.relationship(person1, person2)
     for person1 in family.members.values()
     for person2 in family.members.values()
 )
+
 for r in relationships:
     print(
         "\t".join(
